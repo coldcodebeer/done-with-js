02f096df727d9c9736d164e5e8e37cf00d106d28
coldcodebeer <happy_programming@protonmail.com>
2021-06-24 13:50:24 +0800

    chore: format

--- batchingRequests1.js
+++ batchingRequests1.js
@@ -1,3 +1,4 @@
+  
 const API = `https://api.publicapis.org/entries`
 
 let SUBMITING = false
@@ -12,23 +13,31 @@ const sendRequest = (index) => {
       .then(function (myJson) {
         console.log(`response: ${index}`, myJson)
         resolve()
+      }).catch(err => {
+        console.error(`request_${index}_error=>`, err)
       })
   })
 }
 
-const multiReqs = Array(10).fill(sendRequest)
+const fetchMultiReqs = async (num = 4) => {
+  if (SUBMITING) return
+
+  num > 10 ? num = 10 : null
+
+  const multiReqs = Array(num).fill(sendRequest)
+  try {
+    console.log('-- sending batch --')
+    SUBMITING = true
+    await Promise.all(multiReqs.map((f, i) => f((i + 1))))
+    SUBMITING = false
+    console.log('all asyn code done', 'SUBMITING_', SUBMITING)
+  } catch (err) {
+    console.error('Concurrent_request_error=>', err)
+  }
+}
+
+
 
 const sendRequestsTimerId = setInterval(() => {
-  if (SUBMITING) return
-  ; (async function () {
-    try {
-      console.log('-- sending batch --')
-      SUBMITING = true
-      await Promise.all(multiReqs.map((f, i) => f((i + 1))))
-      SUBMITING = false
-      console.log('all asyn code done', 'SUBMITING_', SUBMITING)
-    } catch (err) {
-      console.error('Concurrent_request_error=>', err)
-    }
-  })()
+  fetchMultiReqs(100)
 }, 2000)
